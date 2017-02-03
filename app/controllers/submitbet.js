238aeee6c02061b3b1131ede18a3a064dc1d9a4f
Rocco Farrell <farrell.roc@gmail.com>
2017-02-02 18:58:47 -0700

    Added submitbet, display bets on sweet cards

--- app/controllers/submitbet.js
+++ app/controllers/submitbet.js
@@ -4,10 +4,12 @@ export default Ember.Controller.extend({
   sortProperties: ['timestamp'],
   sortAscending: false, // sorts post by timestamp
   actions: {
-    publishBet: function() {
+    submitbet: function() {
       var newBet = this.store.createRecord('bets', {
         amount: this.get('amount'),
         bettor: this.get('bettor'),
+        acceptor: this.get('acceptor'),
+        description: this.get('description'),
         timestamp: new Date().getTime()
       });
       newBet.save();
