5c1c30c7e2d2ed042d9d858b519862c96ea7e919
Josh Martin <josh.martin819@gmail.com>
2017-02-06 19:23:52 -0500

    Added barebones navigation and cleared out form on successful post.

--- app/controllers/submitbet.js
+++ app/controllers/submitbet.js
@@ -7,9 +7,13 @@ export default Ember.Controller.extend({
     submitbet: function() {
       var newBet = this.store.createRecord('bets', {
         amount: this.get('amount'),
+        amountset: this.set('amount',''),
         bettor: this.get('bettor'),
+        bettorset: this.set('bettor',''),
         acceptor: this.get('acceptor'),
+        acceptorset: this.set('acceptor',''),
         description: this.get('description'),
+        descriptionset: this.set('description',''),
         timestamp: new Date().getTime()
       });
       newBet.save();
